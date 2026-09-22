903640b73b741c785c0044d64826921adfc2091e
Gaurank Verma <gaurankverma33@gmail.com>
2026-09-22 22:37:36 +0530

    complete project job board

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://job-board-backend-hdle.onrender.com/api",
+  baseURL: "https://job-board-backend-hd1e.onrender.com/api",
 });
 
 export default API;
